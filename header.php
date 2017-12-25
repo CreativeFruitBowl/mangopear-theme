5eb21bbd468e75e7feb0523a7f2de80425126762
Andi North <andi@mangopear.co.uk>
2017-12-25 15:35:31 +0000

    [Global] Removed Google Ads

--- header.php
+++ header.php
@@ -58,15 +58,6 @@
 		
 
 		<?php wp_head(); ?>
-
-
-		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<script>
-			(adsbygoogle = window.adsbygoogle || []).push({
-				google_ad_client: "ca-pub-1323095336194826",
-				enable_page_level_ads: true
-			});
-		</script>
 	</head>
 
 
