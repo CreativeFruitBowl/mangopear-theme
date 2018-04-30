3b89cabb0bb0e6e2bd326cfe086ec2e99bc6d662
Mangopear creative <andi@mangopear.co.uk>
2018-04-30 15:52:35 +0100

    Improved formatting of portfolio header

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -19,7 +19,17 @@
 ?>
 
 
-	<header class="u-clearfix  o-header--portfolio" style="margin-bottom: 3em; background-color: #1CB6C3"><div class="c-portfolio-header__gradient" style="background: #1CB6C3;background: -webkit-radial-gradient(center,ellipse cover,#7FD5E0 0,#1CB6C3 65%);background: -webkit-radial-gradient(center,ellipse,#7FD5E0 0,#1CB6C3 65%);background: radial-gradient(ellipse at center,#7FD5E0 0,#1CB6C3 65%);filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#7FD5E0',endColorstr='#1CB6C3',GradientType=1);"></div>
+	<header class="u-clearfix  o-header--portfolio" style="margin-bottom: 3em; background-color: #1CB6C3">
+		<div class="c-portfolio-header__gradient" 
+		     style="background: #1CB6C3;
+		            background: -webkit-radial-gradient(center, ellipse cover, #7FD5E0 0, #1CB6C3 65%);
+		            background: -webkit-radial-gradient(center, ellipse,       #7FD5E0 0, #1CB6C3 65%);
+		            background:         radial-gradient(ellipse at center,     #7FD5E0 0, #1CB6C3 65%);
+		            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#7FD5E0', endColorstr='#1CB6C3', GradientType=1);
+		     ">
+		</div>
+
+
 		<div class="c-portfolio-header__content">
 			<h1 class="c-portfolio-header__title">Our work</h1>
 			<h2 class="c-portfolio-header__title--sub">Mangopear creative</h2>
