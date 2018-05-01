c5edcdcbadb7554b6fc2000266df300ea81f5b9c
Mangopear creative <andi@mangopear.co.uk>
2018-05-01 18:23:58 +0100

    [404] Updated content of template

--- 404.php
+++ 404.php
@@ -31,7 +31,7 @@
 			<div class="o-container  o-container--optimise-readability  o-container--align-left">
 				<h2>It looks as though there's an issue here.</h2>
 				<p>To be precise, it's an error with the status code 404. <strong>That means the page you tried visiting does not exist.</strong></p>
-				<p>We would suggest checking the link you tried visiting to make sure it looks correct and is spelt correctly.</p>
+				<p>We'd suggest checking the URL you tried visiting to make sure it looks correct and is spelt correctly.</p>
 				<p>If you still can't access the content you're looking for, let us know <a href="https://twitter.com/MangopearUK" target="_blank">on Twitter</a> or by <a href="/contact/">contacting us</a>. Alternatively, you could search our website to find the content you're looking for.</p>
 			</div><!-- /.o-container -->
 		</div><!-- /.o-container -->
