bd03838e998b9ac232ba10c5c7987ab9e30ad73e
Mangopear creative <andi@mangopear.co.uk>
2018-08-30 20:49:38 +0100

    [Global] Switched out logo in header to be a template partial to allow for better theming

--- header.php
+++ header.php
@@ -181,10 +181,7 @@
 
 
 
-			<a href="<?php echo get_site_url(); ?>" class="c-main-header__button  c-main-header__button--left  o-button  o-button--just-icon  c-main-header__cell  c-main-header__cell--left">
-				<svg class="o-icon  o-icon--logo" height="35" width="35" role="presentation"><use xlink:href="<?php echo MANGOPEAR_SPRITE; ?>#mangopear"/></svg>
-				<span class="u-invisible">Mangopear creative</span>
-			</a>
+			<?php get_template_part('template-partials/logo'); ?>
 
 
 
